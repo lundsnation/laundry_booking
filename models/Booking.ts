c0334226cdd151e5f6644bfade32ec2371ac7e55
Adi <adi@creson.se>
2022-11-08 18:42:19 +0100

    Abstracted logging to backendLogger.ts

--- models/Booking.ts
+++ models/Booking.ts
@@ -1,10 +1,5 @@
-import { dateRangePickerDayClasses } from '@mui/lab';
 import mongoose from 'mongoose'
-
-//These can be added to some config later on to increase modularity.
-const debug = false;
-//const writeToFile = false;
-
+import { addEventLogging } from '../utils/backendLogger'
 
 const BookingSchema = new mongoose.Schema({
     date: Date,
@@ -14,36 +9,8 @@ const BookingSchema = new mongoose.Schema({
 const Booking = mongoose.models.Booking || mongoose.model("Booking", BookingSchema)
 
 //print header for logger [type nanana]
-const addEventListeners = () => {
-  //0 = disconnected, 1 = connected, 2 = connecting, 3 = disconnecting
-// Demonstrate the readyState and on event emitters
 
-//const HEADER = "[ TYPE \t nånting]"
 
-  debug && console.log('[ readyStates: { 0 = disconnected, 1 = connected, 2 = connecting, 3 = disconnecting } ]')
-debug && console.log("readyState:" + mongoose.connection.readyState); //logs 0
-
-mongoose.connection.on('connecting', () => {  
-  console.log('LOGGER: [ Connecting to database! \t\t' + (new Date()).toLocaleString() + " ]")
-  debug && console.log("readyState:" + mongoose.connection.readyState); //logs 2
-});
-
-mongoose.connection.on('connected', () => {
-  console.log('LOGGER: [ Connected to database! \t\t' + (new Date()).toLocaleString() + " ]")  
-  debug && console.log("readyState:" + mongoose.connection.readyState); //logs 1
-});
-
-mongoose.connection.on('disconnecting', () => {
-  console.log('LOGGER: [ Disconnecting from database! \t\t' + (new Date()).toLocaleString() + " ]")  
-  debug && console.log("readyState:" + mongoose.connection.readyState); // logs 3
-});
-
-mongoose.connection.on('disconnected', () => {
-  console.log('LOGGER: [ Disconnected from database! \t\t' + (new Date()).toLocaleString() + " ]")  
-  debug && console.log("readyState:" + mongoose.connection.readyState); //logs 0
-});
-}
-
-addEventListeners();
+addEventLogging();
 
 export default Booking;
--- utils/connection.ts
+++ utils/connection.ts
@@ -4,44 +4,13 @@ import mongoose, { Model } from "mongoose"
 // CONNECTING TO MONGOOSE (Get Database Url from .env.local)
 const { DATABASE_URL } = process.env
 
-
-/**const logReadyState() => {
-  if(mongoose.connection.readyState) == 0) {
-
-  }
-}**/
-//Create DbLogger with write to file functionality and make global values for writeFile like in DATABASE_URL and exapnd for all api endpoints add header format
-export const logRequest = (method: string) => {
-  //0 = disconnected, 1 = connected, 2 = connecting, 3 = disconnecting
-// Demonstrate the readyState and on event emitters
-
-  switch(method) {
-    case 'GET':
-    case 'POST':
-    case 'PUT':
-    case 'DELETE':
-      console.log("LOGGER: [Received " + method + " request \t\t" + new Date().toLocaleString() + "]")
-      break;
-
-    default:
-      console.log("LOGGER: [ Receved unknown " + method + " request \t\t" + new Date().toLocaleString() + "]")
-  } 
-}
-
-
-
 // connection function
 export const connect = async () => {
-
-  //if(mongoose.connection.readyState == 0) {
-  //  logDb();
-  //}
 
   const conn = await mongoose
     .connect((DATABASE_URL as string))
     .catch(err => console.log(err))
-  
-  console.log("--------")
+
 
   
   return { conn }
