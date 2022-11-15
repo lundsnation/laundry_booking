8ee88745477357ef1e13ecca6351ab0c9617d30c
Adam <adam.schlytersonesson@gmail.com>
2022-11-15 22:13:15 +0100

    Refactored next.config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  env: {
-    REACT_APP_SECRET: process.env.REACT_APP_SECRET,
-    REACT_APP_ID: process.env.REACT_APP_ID,
-  }
 }
