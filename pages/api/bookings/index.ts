cbe5a62d08848078019c3e68e5bc62723bd9d8ab
Axelb531 <axel.sneitz@gmail.com>
2023-01-01 19:41:01 +0100

    Optimized API call by utilizing user-session instead of fetching from AUTH0

--- pages/api/bookings/index.ts
+++ pages/api/bookings/index.ts
@@ -4,13 +4,19 @@ import { logRequest } from "../../../utils/backendLogger"
 import { ResponseFuncs } from "../../../utils/types"
 import Booking from '../../../models/Booking'
 import { withApiAuthRequired, getSession } from "@auth0/nextjs-auth0"
-import { getUsers } from '../../../utils/getAuth0Users'
+
+function getMonday(d : Date, weeksAhead : number) {
+  d = new Date(d);
+  var day = d.getDay(),
+      diff = d.getDate() - day + (day == 0 ? -6:1)+ weeksAhead*7; 
+  return new Date(d.setDate(diff));
+}
 
 const handler = withApiAuthRequired(async (req: NextApiRequest, res: NextApiResponse) => {
   //capture request method, we type it as a key of ResponseFunc to reduce typing later
   const method: keyof ResponseFuncs = req.method as keyof ResponseFuncs
   const session = await getSession(req, res)
-  const user = session?.user.name
+  const user = session?.user
   const catcher = (error: Error) => res.status(400).json({ error })
   await connect()
 
@@ -21,18 +27,25 @@ const handler = withApiAuthRequired(async (req: NextApiRequest, res: NextApiResp
     },
 
     POST: async (req: NextApiRequest, res: NextApiResponse) => {
-      const userFinder = new getUsers()
+      const userSession = await getSession(req,res)
       const { date, timeSlot, createdAt } = req.body
       // Initial  check if booking-request is in the past => invalid
       if (new Date(date).getTime() < Date.now()) {
         return res.status(406).json({ error: "You cant book slots that are in the past" })
       }
-      const fetchAllowedSlots = await userFinder.getUser("name", user)
-      const allowedSlots = fetchAllowedSlots.app_metadata.allowedSlots
-      const slotCheck = await Booking.find({ userName: user, date: { $gte: new Date() } }).catch(catcher)
-      logRequest('POST');
-      // Checks if user has no bookings or have less than allowed slots already booked in DB
+      // Fetching allowed slots from active user session. If undefined, defaults to 1
+      const allowedSlots = userSession?.user.app_metadata.allowedSlots || 1
+      // Checks if user has a booking in selected week, if now is greater than first day in week, adjusts the interval
+      // const requestDate = req.body.date as Date
+      // const weekStartDate = new Date(getMonday(requestDate,0))
+      // weekStartDate.setHours(0,0,0)
+      // const maxDate = new Date(getMonday(requestDate,1))
+      // maxDate.setHours(0,0,0)
+      // const minDate = weekStartDate<new Date() ? new Date() : weekStartDate
+      // const slotCheck = await Booking.find({ userName: user, date: { $gte: minDate,$lt: maxDate} }).catch(catcher)
+      const slotCheck = await Booking.find({userName: user?.name, date: {$gte: new Date()}})
       if (!slotCheck || slotCheck.length < allowedSlots) {
+        logRequest('POST');
         return res.status(201).json(await Booking.create(req.body).catch(catcher))
       }
       return res.status(400).json({ error: "User has max number of slots booked" })
