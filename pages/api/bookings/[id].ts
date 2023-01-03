6bbb07cb894e07941b9359cda0909c69441eb918
Lunds Nation <lundsnationit@gmail.com>
2023-01-03 11:04:50 +0100

    Revert "Test"

--- pages/api/bookings/[id].ts
+++ pages/api/bookings/[id].ts
@@ -4,20 +4,12 @@ import { logRequest } from "../../../utils/backendLogger"
 import { ResponseFuncs } from "../../../utils/types"
 import Booking from '../../../models/Booking'
 import { withApiAuthRequired, getSession } from '@auth0/nextjs-auth0';
-import Pusher from "pusher"
-
-export const pusher = new Pusher({
-  appId: process.env.PUSHER_APP_ID as string,
-  key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY as string,
-  secret: process.env.PUSHER_SECRET as string,
-  cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER as string,
-});
 
 const handler = withApiAuthRequired(async (req: NextApiRequest, res: NextApiResponse) => {
   const method: keyof ResponseFuncs = req.method as keyof ResponseFuncs
   const catcher = (error: Error) => res.status(400).json({ error })
   const session = await getSession(req, res)
-  const user = session?.user
+  const user = session?.user.name
   // GRAB ID FROM req.query (where next stores params)
   const id: string = req.query.id as string
   // connect to database
@@ -38,12 +30,11 @@ const handler = withApiAuthRequired(async (req: NextApiRequest, res: NextApiResp
     },
     // RESPONSE FOR DELETE REQUESTS WITH VALIDATION, CONFINED TO USER IN ACTIVE SESSION
     DELETE: async (req: NextApiRequest, res: NextApiResponse) => {
-      const query = await Booking.find({ _id: id, userName: user?.name }).catch(catcher)
+      const query = await Booking.find({ _id: id, userName: user }).catch(catcher)
       if (!query) {
         res.status(400).send({ error: 'No bookings for active user' })
       } else {
         res.status(200).json(await Booking.findByIdAndRemove(id).catch(catcher))
-        const pusherRes = await pusher.trigger("laundry-RT","booking-event",{"removedItemId":id});
       }
       logRequest('DELETE')
     },
