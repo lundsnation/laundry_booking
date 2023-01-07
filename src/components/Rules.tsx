fa982beacf299c5e1b147f6b0bfbe2e50700519b
Adi <adi@creson.se>
2023-01-07 02:04:48 +0100

    Added functionality for hiding tooltip when drawer is open

--- src/components/Rules.tsx
+++ src/components/Rules.tsx
@@ -6,7 +6,7 @@ import { positions } from '@mui/system';
 import zIndex from "@mui/material/styles/zIndex";
 import mt from "date-fns/locale/mt";
 import KeyboardDoubleArrowRightIcon from '@mui/icons-material/KeyboardDoubleArrowRight';
- 
+
 
 interface Props {
 
@@ -14,44 +14,50 @@ interface Props {
 
 //{z-index:999;right:0;top:50%;margin-top:-25px;width:50px;height:50px;position:fixed}
 
- const Rules = (props: Props) => {
-    const [open, setOpen] = useState<boolean>(false);
+const Rules = (props: Props) => {
+    const [drawerOpen, setdrawerOpen] = useState<boolean>(false);
 
-    
+
     const toggleDrawer = () => {
-        setOpen(!open);
+        setdrawerOpen(!drawerOpen);
     }
 
 
-    return(
+    const ruleText = (
+        <Typography variant={'button'} sx={{ textOrientation: 'upright', writingMode: 'vertical-lr' }}>
+            Regler
+        </Typography>
+    )
+
+
+
+    return (
         <Box>
-            <Tooltip
-                placement = {'right'}
-                sx = {{ zIndex: 'tooltip', left: 0, top: '40%', position: 'fixed' }}
+            {!drawerOpen && <Tooltip
+                placement={'right'}
+                sx={{ zIndex: 'tooltip', left: 0, top: '40%', position: 'fixed' }}
                 title={<> <KeyboardDoubleArrowRightIcon /></>}
             >
-                <Button variant={'outlined'} onClick={toggleDrawer}>
-                    <Typography>
-                        R <br/> E <br/> G <br/> L <br/> E <br/> R
-                    </Typography> 
+                <Button variant={'text'} onClick={toggleDrawer}>
+                    {ruleText}
                 </Button>
-            </Tooltip> 
+            </Tooltip>}
             <SwipeableDrawer
-            PaperProps={{
-                sx: {
-                    width: 300
-                }
-            }}
-            open={open}
-            onClose={toggleDrawer}
-            onOpen={toggleDrawer}
+                PaperProps={{
+                    sx: {
+                        width: 300
+                    }
+                }}
+                open={drawerOpen}
+                onClose={toggleDrawer}
+                onOpen={toggleDrawer}
             >
                 <Typography>
                     Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione ea, impedit similique adipisci sit praesentium aliquid culpa distinctio voluptatibus accusamus quia cupiditate sapiente nihil inventore obcaecati mollitia repellat veniam esse.
                 </Typography>
-            </SwipeableDrawer> 
-        </Box> 
-    )   
+            </SwipeableDrawer>
+        </Box>
+    )
 }
 
 export default Rules;
