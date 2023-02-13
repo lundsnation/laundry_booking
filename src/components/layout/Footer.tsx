f194a5ebf5bc088bc314a9883c4ca99766209f2f
Adam Sonesson <adam.schlytersonesson@gmail.com>
2023-02-13 22:40:59 +0100

    Relocated columns and typo fix

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -12,31 +12,11 @@ const Footer = () => {
             sx={{
                 backgroundColor: "#afa998", //Får den ej att vilja använda sig av themeProvider t.ex. "secondary"
                 color: "#fff",
-            }
-            }
-        >
+            }}>
 
-            <Grid item sm={12} md={4} display={"flex"} alignItems={"top"} justifyContent={"center"} paddingX={4}
-            >
+            <Grid item sm={12} md={4} display={"flex"} alignItems={"top"} justifyContent={"center"} paddingX={4}>
                 <Grid container >
-                    <Grid item xs={12}>
-                        <Typography display={"flex"} alignItems={"top"} justifyContent={"center"} borderBottom={1} sx={{ fontWeight: "bold" }}>EXPEDITION ÖPPETTIDER </Typography>
-                        <Typography display={"flex"} alignItems={"top"} justifyContent={"center"} fontWeight='medium'>Agardhsgatan 1, 223 51, Lund</Typography>
-                    </Grid>
-                    
-
-                </Grid>
-
-            </Grid>
-
-
-            <Grid item xs={12} sm={4} paddingX={4}>
-                <Grid container spacing={2}>
-                    <Grid item xs={12}>
-                        <Typography display={"flex"} alignItems={"top"} justifyContent={"center"} borderBottom={1} sx={{ fontWeight: "bold" }}>ÖPPETTIDER HUSEXPEN </Typography>
-                        <Typography display={"flex"} alignItems={"top"} justifyContent={"center"} fontWeight='medium'> Mån & Tors 17.00-18.00</Typography>
-                    </Grid>
-                    <Grid item sm={12} md={12} display={"flex"} alignItems={"flex-end"}>
+                    <Grid item sm={12} md={12} display={"flex"}>
                         <Grid item sm={12}>
                             <Typography display={"flex"} alignItems={"top"} justifyContent={"center"} borderBottom={1} fontWeight='bold'>KONTAKT</Typography>
                             <Typography display={"flex"} alignItems={"top"} justifyContent={"center"} fontWeight='medium'>husforman@lundsnation.se </Typography>
@@ -45,13 +25,19 @@ const Footer = () => {
                     </Grid>
                 </Grid>
             </Grid>
+
+            <Grid item xs={12} sm={4} paddingX={4}>
+                <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                        <Typography display={"flex"} alignItems={"top"} justifyContent={"center"} borderBottom={1} sx={{ fontWeight: "bold" }}>ÖPPETTIDER HUSEXPEN </Typography>
+                        <Typography display={"flex"} alignItems={"top"} justifyContent={"center"} fontWeight='medium'> Mån & Tors 17.00-18.00</Typography>
+                        <Typography display={"flex"} alignItems={"top"} justifyContent={"center"} fontWeight='medium'>Agardhsgatan 1, 223 51, Lund</Typography>
+                    </Grid>
+                </Grid>
+            </Grid>
+
             <Grid item xs={12} sm={4} display={"flex"} alignItems={"top"} justifyContent={"center"} paddingX={4}>
                 <Grid container display={"flex"} alignItems={"top"} justifyContent={"center"} rowSpacing={1}>
-
-                    <Grid item xs={12} >
-                        <Typography display={"flex"} alignItems={"top"} justifyContent={"center"} borderBottom={1} fontWeight='bold'>THE TEAM</Typography>
-                    </Grid>
-
                     <Grid item xs={12}
                         display={'flex'}
                         justifyContent={'center'}
@@ -59,21 +45,15 @@ const Footer = () => {
                         sx={{
                             minWidth: 200,
                             maxWidth: 200
-                        }}
-                    >
+                        }}>
 
                         <Link href="/about"><Image src={groupPic} alt={"Image Unavailable"} width={200} /></Link>
 
                     </Grid>
-
                 </Grid>
             </Grid>
-
-
         </Grid >
-
     )
-
 };
 
 export default Footer;
