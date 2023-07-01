9003f38b7d33b310ab4e211fe59c3b3592d16cf0
Adi <adi@creson.se>
2023-07-01 02:21:27 +0200

    Refactored - removed useEffect

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,7 +2,6 @@ import { useUser } from "@auth0/nextjs-auth0/client";
 import { Grid, Paper } from "@mui/material";
 import { NextPage } from "next";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
 import { AboutText } from "../src/components/about/AboutText";
 import Layout from "../src/components/layout/Layout";
 import Loading from "../src/components/Loading";
@@ -11,25 +10,26 @@ const About: NextPage = () => {
     const { user, isLoading, error } = useUser()
     const router = useRouter()
 
-    useEffect(() => {
-        if (!(user || isLoading)) {
-            router.push('api/auth/login')
-        }
-    }, [user, isLoading])
+    if (isLoading) return <Loading />
+    if (error) return <div>{error.message}</div>
+    if (!user) {
+        router.push('/api/auth/login')
+        return null
+    } else {
+        return (
+            <Layout>
+                <Grid container justifyContent="center" >
+                    <Grid item sm={12} md={8} >
 
-    return (user ?
-        <Layout>
-            <Grid container justifyContent="center" >
-                <Grid item sm={12} md={8} >
+                        <Paper variant="outlined" sx={{ my: { sm: 6 }, mx: { xs: 2 } }}>
+                            <AboutText />
+                        </Paper>
 
-                    <Paper variant="outlined" sx={{ my: { sm: 6 }, mx: { xs: 2 } }}>
-                        <AboutText />
-                    </Paper>
-
+                    </Grid >
                 </Grid >
-            </Grid >
-        </Layout> : <Loading />
-    )
+            </Layout>
+        )
+    }
 }
 
 export default About;
