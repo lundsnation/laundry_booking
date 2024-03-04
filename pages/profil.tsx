4d729861b6f342541d4c72d2124cfb516df7953a
Adi <adi@creson.se>
2024-03-04 01:35:09 +0100

    Removed getServerSideProps

--- pages/profil.tsx
+++ pages/profil.tsx
@@ -1,29 +1,44 @@
-import {Grid} from "@mui/material";
-import {withPageAuthRequired} from '@auth0/nextjs-auth0';
-import {NextPage} from "next";
-import {useState} from "react";
-import {Snack, SnackInterface} from
-        "../src/frontend/components/Snack";
+import React, {useState} from 'react';
+import {NextPage} from 'next';
+import {Grid} from '@mui/material';
+import {useUser} from '@auth0/nextjs-auth0/client';
+import {useRouter} from 'next/router';
+
+import Layout from '../src/frontend/components/layout/Layout';
+import Loading from "../src/frontend/components/Loading"; // Assuming you have a Loading component
 import EditProfile from "../src/frontend/components/profile/EditProfile";
-import Layout from "../src/frontend/components/layout/Layout";
+import {Snack, SnackInterface} from "../src/frontend/components/Snack";
 import User, {JsonUser} from "../src/frontend/models/User";
 
-interface Props {
-    user: JsonUser
-}
+const Profile: NextPage = () => {
+    const {user, isLoading, error} = useUser();
+    const router = useRouter();
 
-const Profile: NextPage<Props> = ({user}: Props) => {
-
-    const currentUser = new User(user, [])
     const [snack, setSnack] = useState<SnackInterface>({
         show: false,
         snackString: "",
-        severity: "success"
-    })
+        severity: "success",
+    });
 
     const resetSnack = () => {
-        setSnack({show: false, snackString: snack.snackString, severity: snack.severity})
+        setSnack({show: false, snackString: snack.snackString, severity: snack.severity});
+    };
+
+    // Redirect to login if not authenticated
+    if (!isLoading && !user) {
+        router.push('/api/auth/login');
+        return null; // or a <Loading /> component for a better UX until redirection kicks in
     }
+
+    if (error) {
+        throw error;
+    }
+
+    if (isLoading) {
+        return <Loading/>; // Show loading screen while user info is loading
+    }
+
+    const currentUser = new User(user as JsonUser, []);
 
     return (
         <Layout user={currentUser}>
@@ -32,13 +47,9 @@ const Profile: NextPage<Props> = ({user}: Props) => {
                 <Grid item xs={12} mx={2} my={'10%'}>
                     <EditProfile initUser={currentUser} setSnack={setSnack}/>
                 </Grid>
-
             </Grid>
         </Layout>
-    )
-}
+    );
+};
 
-
-export const getServerSideProps = withPageAuthRequired();
-
-export default Profile
+export default Profile;
